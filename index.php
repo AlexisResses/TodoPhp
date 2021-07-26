0d491d76ddf52f6b1ca51757ae59ddcab709b542
AlexisResses <alexis.resses@epitech.eu>
2021-07-26 16:51:57 +0200

    add input for todo

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
 </head>
 <body>
     <div class="container">
-       <?php require_once 'includes/header.php' ?>
+        <?php require_once 'includes/header.php' ?>
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <div class="todo-form"></div>
+                <form action="/" method="POST" class="todo-form">
+                    <input type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
                 <div class="todo-list"></div>
             </div>
         </div>
